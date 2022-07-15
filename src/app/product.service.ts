f317fb3da61e45b202e15c9a9675613bc6c022dd
twigpi <55249972+twigpi@users.noreply.github.com>
2022-07-15 12:10:24 -0400

    add album and track interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,9 +11,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  // checkpoint 1 Observable<Response>
-  getAlbum(id : number) {
+  getAlbum(id : number): Observable<Album> {
     return this._http.get(this._albumUrl)
-      .map( response => response.json() );
+      .map( <Album>(response) => response.json() );
   }
 }
